a11f9e1f663d512effdd1a860f5969271be7a6e1
Hemant Kumar <itshemant566@gmail.com>
2024-10-11 13:52:24 +0530

    sd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: "tw-",
-  content: ["./src/**/*.{css}", "./partials/*.{php}", "*.{php,css,js}"],
+  content: ["./src/**/*.css", "./partials/*.{php}", "*.{php,css,js}"],
   theme: {
     extend: {
       colors: {
