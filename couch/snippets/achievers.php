b672bbc340b125db0a2083060c6916e805bb215e
Hemant Kumar <itshemant566@gmail.com>
2024-10-11 13:16:10 +0530

    sd

--- couch/snippets/achievers.php
+++ couch/snippets/achievers.php
@@ -70,7 +70,7 @@
                             </div>
                         </div>
                     </div>
-                    <h2 class="tw-font-bold tw-text-3xl ">Cars Achievers</h2>
+                    <h2 class="tw-font-bold tw-mt-10 tw-text-3xl ">Cars Achievers</h2>
                     <div class="row">
                         <div class="col-12">
                             <div class="main">
@@ -128,7 +128,7 @@
                             </div>
                         </div>
                     </div>
-                    <h2 class="tw-font-bold tw-text-3xl ">Tour Package Achievers</h2>
+                    <h2 class="tw-font-bold tw-mt-10 tw-text-3xl ">Tour Package Achievers</h2>
                     <div class="row">
                         <div class="col-12">
                             <div class="main">
