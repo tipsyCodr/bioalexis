9785afc366a74d9d587e12af111fa7bacbc432d5
Hemant Kumar <itshemant566@gmail.com>
2024-10-11 13:12:03 +0530

    dsd

--- couch/snippets/achievers.php
+++ couch/snippets/achievers.php
@@ -11,11 +11,14 @@
     <!-- Start Gallery Us -->
     <section class="news section">
         <div class=" container">
+            <h2 class="tw-font-bold tw-text-3xl ">All Achievers</h2>
+
             <div class="row">
                 <div class="col-lg-8 col-12">
                     <div class="row">
                         <div class="col-12">
                             <div class="main">
+
                                 <cms:pages masterpage='achievers.php' paginate='1' limit='3'>
                                     <div class="row">
 
@@ -67,11 +70,66 @@
                             </div>
                         </div>
                     </div>
-                    <h2>Cars Achievers</h2>
+                    <h2 class="tw-font-bold tw-text-3xl ">Cars Achievers</h2>
                     <div class="row">
                         <div class="col-12">
                             <div class="main">
                                 <cms:pages masterpage='achievers.php' paginate='1' folder="car" limit='3'>
+                                    <div class="row">
+
+                                        <div class="col-lg-4">
+                                            <!-- News thumbnail -->
+                                            <div class="fill news-thumbnail">
+                                                <img src="<cms:show achievers_image/>" alt="#">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <h1 class="news-title"><a href="<cms:show k_page_link/>"><b>
+                                                        <cms:show k_page_title />
+                                                    </b> </a></h1>
+                                            <br>
+                                            <cms:excerpt count="10" ignore="img">
+                                                <cms:show achievers_content />
+                                            </cms:excerpt>
+                                            <!-- Meta -->
+                                            <div class="meta">
+                                                <div class="meta-left">
+                                                    <span class="author"><a href="#"><img src="img/author1.jpg"
+                                                                alt="#">Admin</a></span>
+                                                    <span class="date"><i class="fa fa-clock-o"></i>
+                                                        <cms:date k_page_date format='jS M, y' />
+                                                    </span>
+                                                    <span class="comments"><a href="#"><i class="fa fa-comments"></i>
+                                                            <cms:show k_comment_count />
+                                                        </a></span>
+                                                    <!-- <span class="views"><i class="fa fa-eye"></i> 33K </span> -->
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <hr />
+                                    </div>
+                                    <cms:if k_paginated_bottom>
+                                        <div style="width:100%; text-align:center; padding:20px;color:green;">
+                                            <span style="">
+                                                <cms:if k_paginated_link_prev><a
+                                                        href="<cms:show k_paginated_link_next/>">
+                                                        < Previous Posts</a> | </cms:if>
+                                                <cms:if k_paginated_link_next><a
+                                                        href="<cms:show k_paginated_link_prev/>"> Newer Posts ></a>
+                                                </cms:if>
+                                            </span>
+                                        </div>
+                                    </cms:if>
+                                </cms:pages>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 class="tw-font-bold tw-text-3xl ">Tour Package Achievers</h2>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="main">
+                                <cms:pages masterpage='achievers.php' paginate='1' folder="tour" limit='3'>
                                     <div class="row">
 
                                         <div class="col-lg-4">
