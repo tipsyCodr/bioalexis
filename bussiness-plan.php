c2e3a89b469ba9fc818df77ba48f8f5c1487db1f
Hemant Kumar <itshemant566@gmail.com>
2024-10-15 16:13:31 +0530

    asdasd

--- bussiness-plan.php
+++ bussiness-plan.php
@@ -95,7 +95,8 @@
                 </cms:editable>
             </p>
         </div>
-        <div style="position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden;">
+        <div
+            style="padding: 20px ;max-width: 860px; position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden;">
             <iframe src="https://www.youtube.com/embed/ceqY61gx41g?si=yx5Z61wvhiY2QZRK" title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
