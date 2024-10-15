f0dbda4739eebe08923d382565a945570b1593f0
Hemant Kumar <itshemant566@gmail.com>
2024-10-15 17:20:59 +0530

    sd

--- popup.php
+++ popup.php
@@ -1,6 +1,7 @@
+<?php require_once('couch/cms.php'); ?>
+
 <cms:template title='Change Popup Image' clonable='1'>
     <cms:editable name='image' label='Image' type='image' show_preview='1' preview_height='200' />
-    <cms:editable name='title' label='Title' type='text' />
     <cms:editable name="gg_thumb" assoc_field="image" label="Image Thumbnail" desc="Thumbnail of image above"
         width='200' height='200' enforce_max='1' type="thumbnail" />
 </cms:template>
@@ -16,14 +17,17 @@
 
 <body>
     <?php require_once 'partials/navbar.php' ?>
+    <cms:pages masterpage='popup.php'>
+        <h1 class="tw-font-bold tw-text-2xl">
+            <cms:show title />
+        </h1>
+        <img src="<cms:show image />" alt="<cms:show title />">
 
-    <h1 class="tw-font-bold tw-text-2xl">
-        <cms:show title />
-    </h1>
-    <img src="<cms:show image />" alt="<cms:show title />">
+    </cms:pages>
 
     <?php require_once 'partials/scripts.php' ?>
     <?php require_once 'partials/footer.php' ?>
 </body>
 
 </html>
+<?php COUCH::invoke(); ?>
