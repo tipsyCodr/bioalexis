a04b0878aa89aa90c8b68911a45019afdfca170e
Hemant Kumar <itshemant566@gmail.com>
2024-10-15 18:39:48 +0530

    sd

--- index.php
+++ index.php
@@ -307,7 +307,7 @@ require_once('couch/cms.php'); ?>
 											<cms:pages masterpage='testimonials.php' folder="videos_testimonials"
 												paginate='1' limit='30'>
 												<div
-													class="card testimonial-item tw-shadow tw-p-4 tw-rounded tw-mb-4 tw-bg-white">
+													class="card testimonial-item tw-shadow  tw-rounded tw-mb-4 tw-bg-white">
 
 													<h3 class="tw-text-lg tw-font-semibold tw-mt-2">
 													</h3>
