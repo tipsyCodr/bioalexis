7f9c217ccbdcca5a7458cb23ee7a74cafc5470ef
Hemant Kumar <itshemant566@gmail.com>
2024-10-16 13:47:43 +0530

    d

--- partials/footer.php
+++ partials/footer.php
@@ -101,7 +101,7 @@
 										class="fa fa-caret-right"></i> Terms & Conditions</a></li>
 							<li><a href="downloads.php" class="tw-text-white hover:tw-text-gray-400"><i
 										class="fa fa-caret-right"></i> Download</a></li>
-							<li><a href="grievance.php" class="tw-text-white hover:tw-text-gray-400"><i
+							<li><a href="greivance.php" class="tw-text-white hover:tw-text-gray-400"><i
 										class="fa fa-caret-right"></i> Grievance</a></li>
 							<li><a href="gallery.php" class="tw-text-white hover:tw-text-gray-400"><i
 										class="fa fa-caret-right"></i> Gallery</a></li>
