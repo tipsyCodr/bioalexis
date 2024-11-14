ef54f062c9eee3fcce48177edfc4a096278d9db3
Hemant Kumar <itshemant566@gmail.com>
2024-11-14 13:00:29 +0530

    sd

--- index.php
+++ index.php
@@ -323,7 +323,8 @@ require_once('couch/cms.php'); ?>
 													</div>
 												</div>
 											</cms:pages>
-											<a href="videos_testimonials.php" class="tw-block tw-text-white">
+											<a href="videos_testimonials.php"
+												class="tw-block tw-h-full tw-w-full tw-text-white">
 												<div style=" flex-shrink: 0;border-radius: 8px;padding-left: 20px; padding-right: 20px; padding-bottom: 10px;box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"
 													class="tw-px-4 tw-flex tw-items-center tw-font-bold tw-justify-center tw-bg-gray-400 testimonial-item tw-shadow tw-rounded tw-mb-4 ">
 													View More
